032814c61fa79c85e9c149994f319a3302840950
amanchaurasiya7565 <amanchaurasiya7565@gmail.com>
2026-07-23 11:53:56 +0530

    initial commit

---
+++ lab-1/hello.js
@@ -0,0 +1,14 @@
+console.log("Aman chaurasiya");
+
+//write a function to take two number and return
+//their sum
+function sum(a,b) {
+    return a + b;
+}
+console.log(sum(3, 4));
+const add = (a, b) => {
+    return a + b;
+};
+const add2 = (a, b) => a + b;
+console.log(add(10, 20));
+console.log(add2(10, 30));
