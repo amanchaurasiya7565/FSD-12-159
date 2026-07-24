80d48c5bb4632595cb2049406edbfcef7016face
amanchaurasiya7565 <amanchaurasiya7565@gmail.com>
2026-07-24 14:13:25 +0530

    initial commit

---
+++ lab-1/fun.js
@@ -0,0 +1,6 @@
+// waf to take any digit (0-9) and return its in word as like 0 -one ,5-five .
+const toWord = (digit) => {
+    const words = ["Zero", "One", "two", "three", "Four", "five", "six", "sevem", "Eight", "Nine"];
+    return words[digit];
+}
+console.log(toWord(5));
