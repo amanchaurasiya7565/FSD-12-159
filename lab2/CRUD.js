78674bb30f62a94d7c08be0a4ae8b3f7a6781749
amanchaurasiya7565 <amanchaurasiya7565@gmail.com>
2026-08-14 21:57:40 +0530

    cart

--- lab2/CRUD.js
+++ lab2/CRUD.js
@@ -1,6 +1,34 @@
 import  Readline  from "readline/promises";
-import { stdin,stdout } from "process";
+import { stdin, stdout } from "process";
+import { readFile,writeFile } from "fs/promises";
 
+const FILE = 'product.json'
+
+const getCart = async () => {
+    const data = await readFile(FILE, "utf-8");
+    return JSON.parse(data);
+};
+
+const saveCart = async (myCart) => {
+    await writeFile(FILE, JSON.stringify(myCart, null, 2));
+    
+};
+const addTOCart = async (product) => {
+    const myCart = await getCart();
+    const isFound = myCart.find((item) => item.id === product.id);
+    if (isFound) {
+        isFound.qty += product.qty;
+    } else {
+        myCart.push(product);
+
+    }
+    await saveCart(myCart);
+    console.log(`product added/ updated with id ${product.id} into cart`);
+};
+const showCart = async () => {
+    const data = await getCart();
+    console.table(data);
+}
 const main = async() => {
     let choice;
     const cin = Readline.createInterface({ input: stdin, output: stdout });
@@ -14,11 +42,20 @@ const main = async() => {
         choice = await cin.question("Enter your choice :");
         switch (Number(choice)) {
             case 1:
-                console.log('show product');
+                await showCart();
                 break;
             case 2:
-                console.log('Product Added');
+                let data = await cin.question("Enter id, name, price ,qty :");
+                const [id, name, price, qty] = data.split(',').map((item) => item.trim());
+                const product = {
+                    id: Number(id),
+                    name,
+                    price: Number(price),
+                    qty: Number(qty),
+                };
+                await addTOCart(product);
                 break;
+            
             case 3:
                 console.log('Remove product');
                 break;
